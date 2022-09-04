7e557e3f3dd5cad2a2a9d2a813de72591d3f14a7
Mykyta Lukianitca <nikitalx0@gmail.com>
2022-09-04 13:57:01 +0300

    First working prototype

--- game_words.py
+++ game_words.py
@@ -1,13 +1,58 @@
 class Session:
-    def __init__(self, players_list):
-        self.game_data = {}
-        self.pl_list = players_list
+    def __init__(self, players_list=None):
+        self.game_data = {"counter": 0,
+                          "players_list": {key.name: [] for key in players_list},
+                          "all_words": []}
+
+    def register_player_step(self, player_name: str, word: str):
+        self.game_data["counter"] += 1
+        self.game_data["players_list"][player_name].append(word)
+        self.game_data["all_words"].append(word)
+
+    def save_session(self):
+        pass
+
+    def __str__(self):
+        return self.game_data
+
+    # @staticmethod
+    # def load_session():
+    #     session = Session()
+    #     session.game_data = {}
+    #     return session
 
 
 class Player:
     def __init__(self, name: str):
         self.words = 0
         self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class GameValidation:
+    def __init__(self, to_check: str):
+        self.to_check = to_check.lower()
+
+    def check_name(self):
+        bad_symb = {"'", '"', "/", ",", ">"}  # just example
+        if not self.to_check or set(self.to_check) & bad_symb:
+            return False
+        return True
+
+    def check_word(self, word_list: list):
+        if self.to_check in word_list or self.to_check[0] != word_list[-1][-1]:
+            return False
+        return True
+
+
+class GameOver(Exception):
+    def __init__(self, loser):
+        self.loser = loser
+
+    def __str__(self):
+        return f"Game is over. {self.loser} lost"
 
 
 def players() -> list:
@@ -16,22 +61,49 @@ def players() -> list:
         name_inp = input("Input player name(start for game): ")
         if name_inp.lower() == "start":
             return players_list
-        elif not name_inp:
-            print("Player's name can't be empty. Try again")
+        elif not GameValidation(name_inp).check_name():
+            print("Bad player name. Try again")
         else:
             players_list.append(Player(name_inp))
             print(f"Player {name_inp} added. Input next or start")
 
 
-def circle(session: Session, first_word=False):
-    for player in session.pl_list:
-        if first_word:
-            word = input(f"Okay, {player}, input first word: ")
-        # else:
+def circle(session: Session):
+    pl_list = list(session.game_data['players_list'].keys())
+    word_list = session.game_data['all_words']
+    for player in pl_list:
+        word = input(f"{player}, input word: ").lower()
+        if word == "stop game":
+            raise GameOver(player)
+        if word == "game later":
+            session.save_session()
+        if not word_list:
+            session.register_player_step(player, word)
+        else:
+            word_isvalid = GameValidation(word).check_word(word_list)
+            if word_isvalid:
+                session.register_player_step(player, word)
+            else:
+                raise GameOver(player)
+    return True
 
 
 def game():
-    session = Session(players())
-    return "Game started!"
+    start_game = input("Start new game(new), or load previous(prev):").lower()
+    if start_game == "new":
+        session = Session(players())
+    elif start_game == "prev":
+        session = Session.load_session()
+    else:
+        print("Try start again.")
+        return
+    while True:
+        try:
+            circle(session)
+        except GameOver as e:
+            print(e)
+            return
 
 
+if __name__ == '__main__':
+    game()
